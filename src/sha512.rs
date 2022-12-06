e10cdc887301b8ac8a31c6eebf762b74083697ba
pshkqs <pashamail79@gmail.com>
2022-12-06 13:10:44 +0300

    to_str fn

--- src/sha512.rs
+++ src/sha512.rs
@@ -7,21 +7,26 @@ pub fn gen<TString>(source: TString) -> String
 where
   TString: AsRef<str>,
 {
-  let len = ALPHABET.len();
   let bytes = hash(source.as_ref().as_bytes());
-  let mut res = String::with_capacity(512 >> 3);
-  for byte in bytes {
-    let index = byte as usize - (len * (byte as usize / len));
-    let ch = ALPHABET[index] as char;
-    res.push(ch)
-  }
-
-  res
+  to_str(&bytes)
 }
 
 pub fn gen_rnd() -> String {
   let source: String = repeat_with(|| fastrand::alphanumeric()).take(32).collect();
   gen(source)
+}
+
+pub fn to_str(bytes: &[u8]) -> String {
+  let len = ALPHABET.len();
+  let mut res = String::with_capacity(512 >> 3);
+  for &byte in bytes {
+    let byte = byte as usize;
+    let index = byte - (len * (byte / len));
+    let ch = ALPHABET[index] as char;
+    res.push(ch)
+  }
+
+  res
 }
 
 pub fn hash(bytes: &[u8]) -> Vec<u8> {
